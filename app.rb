7a595dd7d14cb186197f10d7c2754f13685d0fca
Thomas Wright <twwright@gmail.com>
2020-02-17 00:09:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,15 +2,17 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reverse' do
+    @string = params["string"]
     erb :reverse
   end
 
   post '/reverse' do
+    @string
     erb :reversed
   end
 
   get '/friends' do
-    # Write your code here!
+    @friends = 
 
   end
 end
